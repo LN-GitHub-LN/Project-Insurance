ede418b21f99091bba6df692a92befc4d3c9358a
LN-GitHub-LN <laxmi.narayan.one@gmail.com>
2023-05-28 09:36:50 +0530

    data transformer

--- Insurance/utils.py
+++ Insurance/utils.py
@@ -5,6 +5,7 @@ from Insurance.config import mongo_client
 import os,sys
 import numpy as np
 import yaml
+import dill
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     """
@@ -27,7 +28,8 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise InsuranceException(e, sys)
-    
+
+ #***********************************## Data_Validation*******************************************   
 def write_yaml_file(file_path,data:dict):
     try:
         file_dir = os.path.dirname(file_path)
@@ -46,3 +48,41 @@ def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
         return df
     except Exception as e:
         raise InsuranceException(e, sys)
+    
+
+#*********************************** Data_Transformation*******************************************
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of utils")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info("Exited the save_object method of utils")
+    except Exception as e:
+        raise InsuranceException(e, sys) from e
+
+    
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise InsuranceException(e, sys) from e
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise InsuranceException(e, sys) from e
